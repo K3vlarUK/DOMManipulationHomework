6c8140ba70d872c74d459ad0a71da212cb3805f1
Kevin McDermott <kmcdermott31@gmail.com>
2019-10-04 11:38:27 +0100

    refactored code

--- codingHomework/JS/app.js
+++ codingHomework/JS/app.js
@@ -1,42 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const handleFormSubmit = function (event) {
-    event.preventDefault();
-
-    const newGameContainer = document.createElement('div');
-
-    const newGameTitle = document.createElement('h4');
-    newGameTitle.textContent = `${event.target.gameTitle.value}`;
-    newGameContainer.appendChild(newGameTitle);
-
-    const newDatePurchased = document.createElement('p');
-    newDatePurchased.textContent = `${event.target.datePurchased.value}`;
-    newGameContainer.appendChild(newDatePurchased);
-
-    const newGameCost = document.createElement('p');
-    newGameCost.textContent = `${event.target.cost.value}`;
-    newGameContainer.appendChild(newGameCost);
-
-    const newSelection = document.createElement('p');
-    newSelection.textContent = `${event.target.completed.value}`;
-    newGameContainer.appendChild(newSelection);
-
-    const newStoredGame = document.querySelector('.games-list');
-    newStoredGame.appendChild(newGameContainer);
-
-    formInput.reset();
-  };
-
   const formInput = document.querySelector('#new-video-game-entry');
   formInput.addEventListener('submit', handleFormSubmit);
 
-  const clearStoredList = function () {
-    const clearDiv = document.querySelector('.games-list');
-    clearDiv.innerHTML = "";
-  }
-
-  const deleteAll = document.querySelector('#button');
+  const deleteAll = document.querySelector('#deleteButton');
   deleteAll.addEventListener('click', clearStoredList);
 
   console.log('JavaScript Loaded');
+
 });
+
+const handleFormSubmit = function (event) {
+
+  event.preventDefault();
+
+  const gameListItem = createStoredGame(event.target);
+  const newStoredGame = document.querySelector('.games-list');
+  newStoredGame.appendChild(gameListItem);
+
+  event.target.reset();
+
+};
+
+const createStoredGame = function (form) {
+
+  const newGameContainer = document.createElement('div');
+
+  const newGameTitle = document.createElement('h4');
+  newGameTitle.textContent = form.gameTitle.value;
+  newGameContainer.appendChild(newGameTitle);
+
+  const newDatePurchased = document.createElement('p');
+  newDatePurchased.textContent = form.datePurchased.value;
+  newGameContainer.appendChild(newDatePurchased);
+
+  const newGameCost = document.createElement('p');
+  newGameCost.textContent = form.cost.value;
+  newGameContainer.appendChild(newGameCost);
+
+  const newSelection = document.createElement('p');
+  newSelection.textContent = form.completed.value;
+  newGameContainer.appendChild(newSelection);
+
+  return newGameContainer;
+
+};
+
+const clearStoredList = function (event) {
+  const clearDiv = document.querySelector('.games-list');
+  clearDiv.innerHTML = "";
+};
